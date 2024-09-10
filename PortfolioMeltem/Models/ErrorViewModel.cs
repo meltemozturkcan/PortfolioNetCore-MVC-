ddd8614102821c82b123dc2398c01de1a0f2fe32
Meltem Öztürkcan <31880886+meltemozturkcan@users.noreply.github.com>
2024-09-10 10:26:16 +0300

    Add project files.

---
+++ PortfolioMeltem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioMeltem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
